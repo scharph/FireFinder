f51a94678031280fadee7b4de9e1ed096a562591
Philipp Schartlmüller <schartiph@gmail.com>
2020-02-21 15:37:15 +0100

    delete Main

---
+++ FireFinder.Logic/Models/Geo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace FireFinder.Logic.Models
+{
+    public class Geo
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Type { get; set; }
+    }
+}
