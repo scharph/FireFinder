7fa5856bd0cee9b237050c23d79a917def97cd49
Philipp Schartlmüller <schartiph@me.com>
2020-02-26 00:02:58 +0100

    init: add ts api

---
+++ FireFinder.TypeScriptApiClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NSwag;
+using NSwag.CodeGeneration.TypeScript;
+
+namespace FireFinder.TypeScriptApiClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var document = OpenApiDocument.FromUrlAsync("https://firefinder.azurewebsites.net/swagger/v1/swagger.json");
+            //var document = OpenApiDocument.FromUrlAsync("https://localhost:5000/swagger/v1/swagger.json");
+
+            var settings = new TypeScriptClientGeneratorSettings
+            {
+                ClassName = "{controller}Client",
+            };
+
+            var generator = new TypeScriptClientGenerator(await document, settings);
+            var code = generator.GenerateFile();
+
+            string path = "firefinder-api.service.ts";
+
+            using (StreamWriter sw = File.CreateText(path))
+                sw.WriteLine(code);
+
+            Console.WriteLine("Done");
+
+        }
+    }
+}
